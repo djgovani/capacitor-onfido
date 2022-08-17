6363358e6872d9fd5c9465d7226e1d416771e565
Deep Govani <deepgovani97@gmail.com>
2022-08-17 10:34:36 +0530

    add start function and validation error message

--- src/web.ts
+++ src/web.ts
@@ -1,10 +1,56 @@
 import { WebPlugin } from '@capacitor/core';
 
-import type { OnfidoPlugin } from './definitions';
+import { OnfidoCaptureType, OnfidoConfig, OnfidoDocumentType, OnfidoPlugin } from './definitions';
 
 export class OnfidoWeb extends WebPlugin implements OnfidoPlugin {
-  async echo(options: { value: string }): Promise<{ value: string }> {
-    console.log('ECHO', options);
-    return options;
+  async start(config: OnfidoConfig): Promise<any> {
+    if (!config) {
+      return this.configError('config is missing');
+    }
+
+    if (!config.sdkToken) {
+      return this.configError('sdkToken is missing');
+    }
+
+    if (!config.flowSteps) {
+      return this.configError('flowSteps configuration is missing');
+    }
+
+    if (config.flowSteps.captureDocument) {
+      if (config.flowSteps.captureDocument.docType && config.flowSteps.captureDocument.countryCode) {
+        return this.configError(
+          'countryCode needs to be a ISO 3166-1 3 letter code if docType is specified',
+        );
+      }
+
+      if (!config.flowSteps.captureDocument.docType && config.flowSteps.captureDocument.countryCode) {
+        return this.configError(
+          'docType needs to be provided if countryCode is specified',
+        );
+      }
+
+      if (config.flowSteps.captureDocument.docType && !(config.flowSteps.captureDocument.docType in OnfidoDocumentType)) {
+        return this.configError('docType is invalid');
+      }
+    }
+
+    if (!config.flowSteps.captureDocument && !config.flowSteps.captureFace) {
+      return this.configError(
+        "flowSteps doesn't include either valid captureDocument options or valid captureFace options",
+      );
+    }
+
+    if (config.flowSteps.captureFace && !(config.flowSteps.captureFace.type in OnfidoCaptureType)) {
+      return this.configError('Capture Face type is invalid');
+    }
+
+    return null as any;
+  }
+
+  configError(message: string) {
+    const error = new Error(message);
+    error.name = 'config_error';
+    console.log(error);
+    return Promise.reject(error);
   }
 }
